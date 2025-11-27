81481c571352e076713fd41164c44cf5029fd29b
mvntahax <syedamuntahapk@gmail.com>
2025-11-27 15:56:00 +0500

    connected the database

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-const app = express()
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+dotenv.config();
+const app = express();
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send('Mingle Mesh Server Running!')
-})
+app.get("/", (req, res) => {
+  res.send("Mingle Mesh Server Running!");
+});
+
+// connect to mongoDB
+connectDB();
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}!`)
+  console.log(`Server running at http://localhost:${PORT}!`);
 });
