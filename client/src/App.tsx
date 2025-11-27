5cfdbbc9fa815316e22a9ef04d20080cbdf72230
mvntahax <syedamuntahapk@gmail.com>
2025-11-27 15:35:16 +0500

    created auth pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
-import { MingleMeshApp } from "./components/mingle-mesh-app"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { MingleMeshApp } from "./components/mingle-mesh-app";
+import { SignUpPage } from "./components/sign-up";
+import { LoginPage } from "./components/login";
 
 export default function App() {
-  return <MingleMeshApp />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MingleMeshApp />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
